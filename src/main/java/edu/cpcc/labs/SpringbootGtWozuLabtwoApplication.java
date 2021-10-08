b2e09280b9ee5902eba2f94282bf5ef47b0129d1
cpccharry <harry.subramaniam@cpcc.edu>
2021-10-07 21:56:50 -0400

    Initial release.

---
+++ src/main/java/edu/cpcc/labs/SpringbootGtWozuLabtwoApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGtWozuLabtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGtWozuLabtwoApplication.class, args);
+	}
+
+}
